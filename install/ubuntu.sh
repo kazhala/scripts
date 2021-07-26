b7a55ed9f03b636e9257807c1eaf721b5adb420b
Kevin Zhuang <kevin7441@gmail.com>
2021-07-26 15:34:30 +1000

    install lua-formatter

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -78,6 +78,10 @@ brew instlal packer
 brew install lua
 brew install luarocks
 
+# -- LUA --------------------------------------------------------------------------
+
+luarocks install --server=https://luarocks.org/dev luaformatter
+
 # -- VIM --------------------------------------------------------------------------
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
